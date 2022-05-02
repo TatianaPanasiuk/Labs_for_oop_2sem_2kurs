2ab00714c2540e243e61344128a845321cbff2b4
TatianaPanasiuk <89963934+TatianaPanasiuk@users.noreply.github.com>
2022-05-02 10:57:17 +0300

    Add files via upload

---
+++ Лаба9/Лаба9/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Лаба9
+{
+    public class Book : DependencyObject
+    {
+        public static readonly DependencyProperty TitleProperty;
+        public static readonly DependencyProperty PriceProperty;
+
+        static Book()
+        {
+            TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Book));
+
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.CoerceValueCallback = new CoerceValueCallback(CorrectValue);
+
+            PriceProperty = DependencyProperty.Register("Price", typeof(int), typeof(Book), metadata,
+                new ValidateValueCallback(ValidateValue));
+        }
+
+        private static object CorrectValue(DependencyObject d, object baseValue)
+        {
+            int currentValue = (int)baseValue;
+            if (currentValue > 600)  // если больше 1000, возвращаем 1000
+                return 600;
+            return currentValue; // иначе возвращаем текущее значение
+        }
+
+        private static bool ValidateValue(object value)
+        {
+            int currentValue = (int)value;
+            if (currentValue >= 0) // если текущее значение от нуля и выше
+                return true;
+            return false;
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public int Price
+        {
+            get { return (int)GetValue(PriceProperty); }
+            set { SetValue(PriceProperty, value); }
+        }
+    }
+}
---
+++ Лаба9/Лаба9/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Лаба9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Z1_Click(object sender, RoutedEventArgs e)
+        {
+            ValidateAndCoerce ValidateAndCoerce = new ValidateAndCoerce();
+            ValidateAndCoerce.Show();
+        }
+
+        private void Z2_Click(object sender, RoutedEventArgs e)
+        {
+            RoutedEventBubblingEvents RoutedEventBubblingEvents = new RoutedEventBubblingEvents();
+            RoutedEventBubblingEvents.Show();
+
+        }
+
+        private void Z3_Click(object sender, RoutedEventArgs e)
+        {
+            TunnelingEvents TunnelingEvents = new TunnelingEvents();
+            TunnelingEvents.Show();
+        }
+ 
+        
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Welcom to WPF LABA 9.\n This is great application");
+        }
+
+        private void button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            button.Content = "Cho-to izmenilos?";
+        }
+
+        private void Z4_Click(object sender, RoutedEventArgs e)
+        {
+            RoutedUICommand RoutedUICommand = new RoutedUICommand();
+            RoutedUICommand.Show();
+        }
+
+        private void button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            button.Content = "pokazalos";
+        }
+    }
+}
